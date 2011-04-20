3e384d71146bf204c587cb1793cb11c86c322ca0
Tobias Raeder <tobias.raeder@freiheit.com>
2011-04-20 22:12:46 +0200

    Some more stuff for polls

--- web/js/pollpage.js
+++ web/js/pollpage.js
@@ -1,14 +1,18 @@
 goog.provide('PollPage');
 
+goog.require('PollDesigner');
+
 goog.require('goog.ui.Component');
 
 
 PollPage = function(opt_domHelper) {
-    goog.base(this, opt_domHelper);
+  goog.base(this, opt_domHelper);
 };
 goog.inherits(PollPage, goog.ui.Component);
 
 PollPage.prototype.createDom = function() {
-    var element = this.dom_.createDom('div', 'poll-page', 'Poll Page');
-    this.setElementInternal(element);
-}
+  var element = this.dom_.createDom('div', 'poll-page', 'Poll Page');
+  this.setElementInternal(element);
+  this.designer_ = new PollDesigner();
+  this.addChild(this.designer_, true);
+};
