9d6054ac5e31edbc9efef3512ab809538d3bc0db
Sebastian Gutsfeld <sebastian.gutsfeld@freiheit.com>
2011-09-21 19:33:30 +0200

    fixed unmatched delimiter

--- src/clj_hh/core.clj
+++ src/clj_hh/core.clj
@@ -15,7 +15,7 @@
   (compojure/GET "/" _   index-handler/show-index)
   (compojure/GET "/profile" _ (session/only-logged-in (session/with-user profile-handler/show-profile)))
   (compojure/GET "/login" _ login-handler/handle-login)
-  (compojure/GET "/logout" _ login-handler/handle-logout))
+  (compojure/GET "/logout" _ login-handler/handle-logout)
   (compojure/POST "/broadcast" _ (session/only-logged-in broadcast-handler/post-broadcast)))
 
 (def clj-hh-app-handler
